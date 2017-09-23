cad0ce2db81dbae78a44771c3817ed0d9f893edc
Carmen Stockberger <carmen.stockberger@gmail.com>
2017-09-23 09:49:34 -0700

    initial site files

---
+++ assets/schedule.js
@@ -0,0 +1,13 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCaXQf3nxouJPwAqVsq5B7fTV_UukTklQU",
+    authDomain: "trainscheduler-efd95.firebaseapp.com",
+    databaseURL: "https://trainscheduler-efd95.firebaseio.com",
+    projectId: "trainscheduler-efd95",
+    storageBucket: "",
+    messagingSenderId: "224848766108"
+  };
+  firebase.initializeApp(config);
+
+
+
